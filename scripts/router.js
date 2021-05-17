10da6c63a80056a8701a8e3c51e3f3cfa1f0e096
m6kang <m6kang@ucsd.edu>
2021-05-16 18:42:56 -0700

    Finished Lab

--- scripts/router.js
+++ scripts/router.js
@@ -5,7 +5,7 @@ export const router = {};
 /**
  * Changes the "page" (state) that your SPA app is currently set to
  */
-router.setState = function() {
+router.setState = function(state) {
   /**
    * - There are three states that your SPA app will have
    *    1. The home page
@@ -35,4 +35,20 @@ router.setState = function() {
    *    1. You may add as many helper functions in this file as you like
    *    2. You may modify the parameters of setState() as much as you like
    */
+  const body = document.querySelector('body');
+  const title = document.querySelector('header h1');
+  if (state.name == 'home') {
+    body.className = '';
+    title.innerHTML = 'Journal Entries';
+  } else if (state.name == 'settings') {
+    body.className = 'settings';
+    title.innerHTML = 'Settings';
+  } else if (state.name == 'entry') {
+    body.className = 'single-entry';
+    title.innerHTML = 'Entry ' + state.id;
+    let newEntry = document.createElement('entry-page');
+    newEntry.entry = document.getElementById(state.id).entry;
+    body.removeChild(document.getElementsByTagName('entry-page')[0]);
+    body.appendChild(newEntry);
+  }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ const setState = router.setState;
 
 // Make sure you register your service worker here too
 
+let id = 0;
 document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
     .then(response => response.json())
@@ -12,7 +13,29 @@ document.addEventListener('DOMContentLoaded', () => {
       entries.forEach(entry => {
         let newPost = document.createElement('journal-entry');
         newPost.entry = entry;
+        id++;
+        newPost.id = id;
+        newPost.addEventListener('click', () => {
+          history.pushState({name: 'entry', id: newPost.id}, '', '#entry' + newPost.id);
+          setState({name: 'entry', id: newPost.id});
+        })
         document.querySelector('main').appendChild(newPost);
       });
     });
 });
+
+let settings = document.querySelector('header img');
+settings.addEventListener('click', () => {
+  history.pushState({name: 'settings', id: 0}, '', '#settings');
+  setState({name: 'settings', id: 0});
+});
+
+let home = document.querySelector('header h1');
+home.addEventListener('click', () => {
+  history.pushState({name: 'home', id: 0}, '', ' ');
+  setState({name: 'home', id: 0});
+});
+
+window.onpopstate = function(event) {
+  setState(event.state);
+};
